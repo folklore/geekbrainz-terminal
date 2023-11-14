fbcea11aeef7c4fd61247cfe14d12ed14dda4c1a
noname <noemail>
2023-11-14 20:10:32 +0500

    Добавит пакеты в requirements

--- brewery_project/settings.py
+++ brewery_project/settings.py
@@ -96,15 +96,12 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'ifolklore$default',
-            'USER': 'ifolklore',
-            'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-            'HOST': 'ifolklore.mysql.pythonanywhere-services.com',
-            'OPTIONS': {
-                'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
-                'charset': 'utf8mb4',
-            },
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'gb_database',
+            'USER': 'gb_user',
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': 'localhost',
+            'POST': 5432
         }
     }
 
